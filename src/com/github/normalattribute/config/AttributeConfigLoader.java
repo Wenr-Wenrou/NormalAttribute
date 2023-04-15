68e6acaaadb3f7e50f8e84c9444cdf3df92a8212
Wenr-Wenrou <127990606+Wenr-Wenrou@users.noreply.github.com>
2023-04-15 12:56:20 +0800

    Create AttributeConfigLoader.java

---
+++ src/com/github/normalattribute/config/AttributeConfigLoader.java
@@ -0,0 +1,386 @@
+package com.github.normalattribute.config;
+
+import java.util.List;
+
+public class AttributeConfigLoader {
+    private String damage;
+    private String denfense;
+    private String armor;
+    private String breakArmor;
+    private String health;
+    private String critChance;
+    private String crit;
+    private String stealChance;
+    private String stealLife;
+    private String dodge;
+    private String hitRate;
+    private String block;
+    private String blockRate;
+    private String ignoreArmor;
+    private String ignoreDefense;
+    private String moveSpeed;
+    private String sellPrice;
+    private String sellPoint;
+    private String percentTag;
+    private String rateTag;
+    private String addTag;
+    private String takeTag;
+    private String globalLevelTag;
+    private String globalLevelSplit;
+    private String globalClassTag;
+    private String globalClassSplit;
+    private List<String> globalMainHandTag;
+    private List<String> globalOffHandTag;
+    private List<String> globalHelmetTag;
+    private List<String> globalChestplateTag;
+    private List<String> globalLeggingsTag;
+    private List<String> globalBootsTag;
+    private String attributeLevelTag;
+    private String attributeLevelSplit;
+    private String attributeClassTag;
+    private String attributeClassSplit;
+    private List<String> attributeMainHandTag;
+    private List<String> attributeOffHandTag;
+    private List<String> attributeHelmetTag;
+    private List<String> attributeChestplateTag;
+    private List<String> attributeLeggingsTag;
+    private List<String> attributeBootsTag;
+
+    public String getDamage() {
+        return damage;
+    }
+
+    public void setDamage(String damage) {
+        this.damage = damage;
+    }
+
+    public String getDenfense() {
+        return denfense;
+    }
+
+    public void setDenfense(String denfense) {
+        this.denfense = denfense;
+    }
+
+    public String getArmor() {
+        return armor;
+    }
+
+    public void setArmor(String armor) {
+        this.armor = armor;
+    }
+
+    public String getBreakArmor() {
+        return breakArmor;
+    }
+
+    public void setBreakArmor(String breakArmor) {
+        this.breakArmor = breakArmor;
+    }
+
+    public String getHealth() {
+        return health;
+    }
+
+    public void setHealth(String health) {
+        this.health = health;
+    }
+
+    public String getCritChance() {
+        return critChance;
+    }
+
+    public void setCritChance(String critChance) {
+        this.critChance = critChance;
+    }
+
+    public String getCrit() {
+        return crit;
+    }
+
+    public void setCrit(String crit) {
+        this.crit = crit;
+    }
+
+    public String getStealChance() {
+        return stealChance;
+    }
+
+    public void setStealChance(String stealChance) {
+        this.stealChance = stealChance;
+    }
+
+    public String getStealLife() {
+        return stealLife;
+    }
+
+    public void setStealLife(String stealLife) {
+        this.stealLife = stealLife;
+    }
+
+    public String getDodge() {
+        return dodge;
+    }
+
+    public void setDodge(String dodge) {
+        this.dodge = dodge;
+    }
+
+    public String getHitRate() {
+        return hitRate;
+    }
+
+    public void setHitRate(String hitRate) {
+        this.hitRate = hitRate;
+    }
+
+    public String getBlock() {
+        return block;
+    }
+
+    public void setBlock(String block) {
+        this.block = block;
+    }
+
+    public String getBlockRate() {
+        return blockRate;
+    }
+
+    public void setBlockRate(String blockRate) {
+        this.blockRate = blockRate;
+    }
+
+    public String getIgnoreArmor() {
+        return ignoreArmor;
+    }
+
+    public void setIgnoreArmor(String ignoreArmor) {
+        this.ignoreArmor = ignoreArmor;
+    }
+
+    public String getIgnoreDefense() {
+        return ignoreDefense;
+    }
+
+    public void setIgnoreDefense(String ignoreDefense) {
+        this.ignoreDefense = ignoreDefense;
+    }
+
+    public String getMoveSpeed() {
+        return moveSpeed;
+    }
+
+    public void setMoveSpeed(String moveSpeed) {
+        this.moveSpeed = moveSpeed;
+    }
+
+    public String getSellPrice() {
+        return sellPrice;
+    }
+
+    public void setSellPrice(String sellPrice) {
+        this.sellPrice = sellPrice;
+    }
+
+    public String getSellPoint() {
+        return sellPoint;
+    }
+
+    public void setSellPoint(String sellPoint) {
+        this.sellPoint = sellPoint;
+    }
+
+    public String getPercentTag() {
+        return percentTag;
+    }
+
+    public void setPercentTag(String percentTag) {
+        this.percentTag = percentTag;
+    }
+
+    public String getRateTag() {
+        return rateTag;
+    }
+
+    public void setRateTag(String rateTag) {
+        this.rateTag = rateTag;
+    }
+
+    public String getAddTag() {
+        return addTag;
+    }
+
+    public void setAddTag(String addTag) {
+        this.addTag = addTag;
+    }
+
+    public String getTakeTag() {
+        return takeTag;
+    }
+
+    public void setTakeTag(String takeTag) {
+        this.takeTag = takeTag;
+    }
+
+    public String getGlobalLevelTag() {
+        return globalLevelTag;
+    }
+
+    public void setGlobalLevelTag(String globalLevelTag) {
+        this.globalLevelTag = globalLevelTag;
+    }
+
+    public String getGlobalLevelSplit() {
+        return globalLevelSplit;
+    }
+
+    public void setGlobalLevelSplit(String globalLevelSplit) {
+        this.globalLevelSplit = globalLevelSplit;
+    }
+
+    public String getGlobalClassTag() {
+        return globalClassTag;
+    }
+
+    public void setGlobalClassTag(String globalClassTag) {
+        this.globalClassTag = globalClassTag;
+    }
+
+    public String getGlobalClassSplit() {
+        return globalClassSplit;
+    }
+
+    public void setGlobalClassSplit(String globalClassSplit) {
+        this.globalClassSplit = globalClassSplit;
+    }
+
+    public List<String> getGlobalMainHandTag() {
+        return globalMainHandTag;
+    }
+
+    public void setGlobalMainHandTag(List<String> globalMainHandTag) {
+        this.globalMainHandTag = globalMainHandTag;
+    }
+
+    public List<String> getGlobalOffHandTag() {
+        return globalOffHandTag;
+    }
+
+    public void setGlobalOffHandTag(List<String> globalOffHandTag) {
+        this.globalOffHandTag = globalOffHandTag;
+    }
+
+    public List<String> getGlobalHelmetTag() {
+        return globalHelmetTag;
+    }
+
+    public void setGlobalHelmetTag(List<String> globalHelmetTag) {
+        this.globalHelmetTag = globalHelmetTag;
+    }
+
+    public List<String> getGlobalChestplateTag() {
+        return globalChestplateTag;
+    }
+
+    public void setGlobalChestplateTag(List<String> globalChestplateTag) {
+        this.globalChestplateTag = globalChestplateTag;
+    }
+
+    public List<String> getGlobalLeggingsTag() {
+        return globalLeggingsTag;
+    }
+
+    public void setGlobalLeggingsTag(List<String> globalLeggingsTag) {
+        this.globalLeggingsTag = globalLeggingsTag;
+    }
+
+    public List<String> getGlobalBootsTag() {
+        return globalBootsTag;
+    }
+
+    public void setGlobalBootsTag(List<String> globalBootsTag) {
+        this.globalBootsTag = globalBootsTag;
+    }
+
+    public String getAttributeLevelTag() {
+        return attributeLevelTag;
+    }
+
+    public void setAttributeLevelTag(String attributeLevelTag) {
+        this.attributeLevelTag = attributeLevelTag;
+    }
+
+    public String getAttributeLevelSplit() {
+        return attributeLevelSplit;
+    }
+
+    public void setAttributeLevelSplit(String attributeLevelSplit) {
+        this.attributeLevelSplit = attributeLevelSplit;
+    }
+
+    public String getAttributeClassTag() {
+        return attributeClassTag;
+    }
+
+    public void setAttributeClassTag(String attributeClassTag) {
+        this.attributeClassTag = attributeClassTag;
+    }
+
+    public String getAttributeClassSplit() {
+        return attributeClassSplit;
+    }
+
+    public void setAttributeClassSplit(String attributeClassSplit) {
+        this.attributeClassSplit = attributeClassSplit;
+    }
+
+    public List<String> getAttributeMainHandTag() {
+        return attributeMainHandTag;
+    }
+
+    public void setAttributeMainHandTag(List<String> attributeMainHandTag) {
+        this.attributeMainHandTag = attributeMainHandTag;
+    }
+
+    public List<String> getAttributeOffHandTag() {
+        return attributeOffHandTag;
+    }
+
+    public void setAttributeOffHandTag(List<String> attributeOffHandTag) {
+        this.attributeOffHandTag = attributeOffHandTag;
+    }
+
+    public List<String> getAttributeHelmetTag() {
+        return attributeHelmetTag;
+    }
+
+    public void setAttributeHelmetTag(List<String> attributeHelmetTag) {
+        this.attributeHelmetTag = attributeHelmetTag;
+    }
+
+    public List<String> getAttributeChestplateTag() {
+        return attributeChestplateTag;
+    }
+
+    public void setAttributeChestplateTag(List<String> attributeChestplateTag) {
+        this.attributeChestplateTag = attributeChestplateTag;
+    }
+
+    public List<String> getAttributeLeggingsTag() {
+        return attributeLeggingsTag;
+    }
+
+    public void setAttributeLeggingsTag(List<String> attributeLeggingsTag) {
+        this.attributeLeggingsTag = attributeLeggingsTag;
+    }
+
+    public List<String> getAttributeBootsTag() {
+        return attributeBootsTag;
+    }
+
+    public void setAttributeBootsTag(List<String> attributeBootsTag) {
+        this.attributeBootsTag = attributeBootsTag;
+    }
+
+
+}
